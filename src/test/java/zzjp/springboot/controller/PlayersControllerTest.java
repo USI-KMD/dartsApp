adb678fc0c99a21ead937aeef23f70b87cc525b9
lukasz.strobin <lukasz.strobin@gmail.com>
2017-03-28 19:06:13 +0200

    added dummy test

--- src/test/java/zzjp/springboot/controller/PlayersControllerTest.java
+++ src/test/java/zzjp/springboot/controller/PlayersControllerTest.java
@@ -1,5 +1,6 @@
 package zzjp.springboot.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,6 +15,10 @@ import static org.junit.Assert.*;
 public class PlayersControllerTest {
 
 
+    @Test
+    public void verifyControllerCalled() {
+        // ... stub
+    }
 
 
 
